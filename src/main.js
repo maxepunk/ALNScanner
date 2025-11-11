42f33c263323336c446e2f43b0b01d6e3c694271
maxepunk <max@maxepunk.com>
2025-11-11 10:26:24 -0800

    feat: create ES6 module directory structure

---
+++ src/main.js
@@ -0,0 +1,11 @@
+/**
+ * ALNScanner - Main Entry Point
+ * ES6 Module Architecture
+ *
+ * This file will orchestrate the application initialization
+ * once all modules are converted.
+ */
+
+console.log('ALNScanner ES6 - Module entry point loaded');
+
+// TODO: Import and initialize app after module conversion
